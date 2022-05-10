25989882840ea2134d1100b5f99a7a724f72284c
dong yang <215387880@qq.com>
2022-05-10 10:57:18 +0800

    test

--- src/pages/home2.jsx
+++ src/pages/home2.jsx
@@ -1,10 +1,10 @@
 /*
  * @Author: your name
  * @Date: 2021-06-09 17:11:07
- * @LastEditTime: 2022-01-25 15:01:05
+ * @LastEditTime: 2022-05-10 10:56:58
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \coded:\myWebpack\src\home.jsx
+ * @FilePath: /click/Users/derbysofti41/vite/src/pages/home2.jsx
  */
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
@@ -19,7 +19,7 @@ export default function Home2() {
       <div>
         <div>{count}</div>
         <button type="button" onClick={() => dispatch(increment())}>
-          Increment
+          Increment112
         </button>
         <button type="button" onClick={() => dispatch(decrement())}>
           Decrement
